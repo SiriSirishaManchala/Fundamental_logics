00a1ff9f32b79ec69976b39b562efb2520eacfc1
Sirisha Manchala <38764576+SiriSirishaManchala@users.noreply.github.com>
2018-08-10 12:02:50 +0530

    Update check input_isAlpha_or_not.js

--- check input_isAlpha_or_not.js
+++ check input_isAlpha_or_not.js
@@ -1,5 +1,12 @@
+
+// 12. Check Whether a Character is an Alphabet or not?
+
 var alphaChar = "3"; 
  console.log("Entered Input is", +alphaChar);
+
+// comparing both the alphabets uppercase and lowrcases,
+//if it satisfied condition the input is alphabet otherwise not alphabet.
+
 if((alphaChar>='a' && alphaChar<='z')||(alphaChar>='A' && alphaChar<='Z'))
     {
     console.log("Given input is a Character" +alphaChar);
@@ -7,3 +14,24 @@ if((alphaChar>='a' && alphaChar<='z')||(alphaChar>='A' && alphaChar<='Z'))
 else{
 console.log("Given Input is not a character" ,+alphaChar)
 }
+
+
+
+                      (or )
+
+// dynamicall checking whether given input is alphabet or not
+var alphaChar = ""; 
+function alphabetOrNot(alphaChar)
+{
+ console.log("Entered Input is " +alphaChar);
+
+if((alphaChar>='a' && alphaChar<='z')||(alphaChar>='A' && alphaChar<='Z'))
+   
+ {
+    console.log("Given input is a Character" +alphaChar);
+}
+
+else{
+    console.log("Given Input is not a character" ,+alphaChar)
+}
+};
