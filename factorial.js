0366e95a5f200e2606a09b17eefa4c646a7f48a1
Sirisha Manchala <38764576+SiriSirishaManchala@users.noreply.github.com>
2018-08-10 11:43:14 +0530

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,10 @@
+var facto = 1; var num = 10;
+ 
+  console.log("Enter a number:"+ num);
+ 
+  for(var i=1;i<=num;i++)
+  {
+      facto = facto*i;
+ 
+  console.log("Factorial of given number is:" +facto);
+  }
