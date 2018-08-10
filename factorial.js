26fe6efab7a0c0bce860656ca15d1fee28805404
Sirisha Manchala <38764576+SiriSirishaManchala@users.noreply.github.com>
2018-08-10 12:08:47 +0530

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,10 +1,9 @@
- //factorial- Number Prints
-
+ // 13. Programm for to find the factorial of a given Number Prints
 
 var facto = 1; 
 var num = 10;
  
-  console.log("Enter a number:"+ num); // optional print statement.
+  console.log("Enter a number:"+ num); // optional input statement.
  
   for(var i=1;i<=num;i++)
   {
@@ -12,3 +11,17 @@ var num = 10;
  
   console.log("Factorial of given number is:" +facto);
   }
+      (or)
+
+var facto = 1; 
+var num = "";
+ Function factorial(num){
+  console.log("Enter a number:" + num); //input assaign dynamically
+ 
+  for(var i=1;i<=num;i++)
+  {
+      facto = facto*i;  // print the given input factors.
+ 
+  console.log("Factorial of given number is:" +facto);
+  }
+ }
