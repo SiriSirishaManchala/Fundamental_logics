4e4a8a2c535d2efef11a28b8873ea481e687d550
Sirisha Manchala <38764576+SiriSirishaManchala@users.noreply.github.com>
2018-08-10 11:17:41 +0530

    Update strPalindrome_logic.js

--- strPalindrome_logic.js
+++ strPalindrome_logic.js
@@ -24,5 +24,23 @@ function checkPalindrome(strng){
 			}else{
 				return 'Not Palindrome';
 			}
-		};
+              };
 		
+	(or)
+		//basic logic Method-2
+
+var str="level";
+var text="";
+var next="";
+
+for(var i=0;i<str.length;i++) {
+text = str[i];
+}
+for(var j=str.length;j>=0;j--) {
+next = str[j];
+}
+if(text==next) {
+console.log("it is a palindrome");
+}else {
+console.log("it is not a palindrome");
+}
