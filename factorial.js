62a265afa01369f78d3e0e7162516ab45dfd1449
Sirisha Manchala <38764576+SiriSirishaManchala@users.noreply.github.com>
2018-08-10 11:47:59 +0530

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,10 +1,14 @@
-var facto = 1; var num = 10;
+ //factorial- Number Prints
+
+
+var facto = 1; 
+var num = 10;
  
-  console.log("Enter a number:"+ num);
+  console.log("Enter a number:"+ num); // optional print statement.
  
   for(var i=1;i<=num;i++)
   {
-      facto = facto*i;
+      facto = facto*i;  // print the given input factors.
  
   console.log("Factorial of given number is:" +facto);
   }
