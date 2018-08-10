5f0b125975e918001aa26ce404bdecf70fe80cd3
Sirisha Manchala <38764576+SiriSirishaManchala@users.noreply.github.com>
2018-08-10 12:57:04 +0530

    Add files via upload

---
+++ vowel_Or_consonant.js
@@ -0,0 +1,21 @@
+var chr="";
+function vowelOrConso(chr)
+{
+ 
+  console.log("Input a character: " +chr);
+ 
+  if ((chr >= 'a' && chr <= 'z') || (chr >= 'A' &&chr <= 'Z')) 
+     {
+         if (chr=='a' || chr=='A' || chr=='e' || chr=='E' || chr=='i' || chr=='I' || chr=='o' || chr=='O' || chr== 'u' || chr=='U')
+         {
+          console.log(" Input is a vowel" +chr);
+         }
+      else{
+          console.log(" Input is a consonant" +chr);
+        }
+        // if give a input is a number use it else statement
+        //else{
+        //     console.log("Given Input is not a vowel or consonant " +chr);
+        // }
+     }
+}
