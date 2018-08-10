07593388346c31c3b8fb64f92d7038cb9a0b63ab
Sirisha Manchala <38764576+SiriSirishaManchala@users.noreply.github.com>
2018-08-10 10:49:15 +0530

    Add files via upload

---
+++ check input_isAlpha_or_not.js
@@ -0,0 +1,9 @@
+var alphaChar = "3"; 
+ console.log("Entered Input is", +alphaChar);
+if((alphaChar>='a' && alphaChar<='z')||(alphaChar>='A' && alphaChar<='Z'))
+    {
+    console.log("Given input is a Character" +alphaChar);
+}
+else{
+console.log("Given Input is not a character" ,+alphaChar)
+}
